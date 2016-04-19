cb53afa0b4b5a172f93b70842784f7bd044839b8
Dan Ray <dray@redlettermarketing.com>
2016-04-19 13:26:31 -0400

    Continuing to try stuf

--- lib/redmine_webhook/time_entry_wrapper.rb
+++ lib/redmine_webhook/time_entry_wrapper.rb
@@ -6,7 +6,14 @@ module RedmineWebhook
 
     def to_hash
       {
-        :id => @time_entry.id
+        :id => @time_entry.id,
+        :hours => @time_entry.hours,
+        :comments => @time_entry.comments,
+        :created_on => @time_entry.created_on,
+        :updated_on => @time_entry.updated_on,
+  #      :project => RedmineWebhook::ProjectWrapper.new(@time_entry.project).to_hash,
+  #      :user => RedmineWebhook::AuthorWrapper.new(@time_entry.user).to_hash,
+   #     :issue => RedmineWebhook::IssueWrapper.new(@time_entry.issue).to_hash
       }
     end
   end
